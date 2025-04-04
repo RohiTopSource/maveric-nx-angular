3b812218cee80d6763d4c1274a95c91e742ecacb
Rohidas Balasaheb Kakad <rohidask@maveric-systems.com>
2025-04-04 18:20:29 +0530

    added login form

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { UserService } from '@angular-monorepo/data-access-user';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { distinctUntilChanged, Observable } from 'rxjs';
 // import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+private userService = inject(UserService);
+private router = inject(Router);
+isLoggedIn$: Observable<boolean> = this.userService.isUserLoggedIn$;
+
+
+ngOnInit(): void {
+    this.isLoggedIn$.pipe(
+      distinctUntilChanged()
+    ).subscribe((loggedIn)=> {
+         setTimeout(()=> {
+          if(!loggedIn){
+              this.router.navigateByUrl('login');
+          }else{
+            this.router.navigateByUrl('');
+          }
+         })
+    })
+}
+
 }
